cfb93115fb3ff25786b06bbfcbffdf819e4c52a1
LazarBojanic <bojanic.lazar50@gmail.com>
2025-10-26 20:45:26 +0100

    fixing up auth

--- backend/internconnectbackendktor/src/main/kotlin/routing/AuthRoutes.kt
+++ backend/internconnectbackendktor/src/main/kotlin/routing/AuthRoutes.kt
@@ -1,7 +1,12 @@
 package com.internconnect.routing
 
+import com.internconnect.dto.LoginUserDto
+import com.internconnect.model.dto.RegisterCompanyDto
+import com.internconnect.model.dto.RegisterStudentDto
 import com.internconnect.service.implementation.AuthService
 import io.ktor.server.auth.*
+import io.ktor.server.request.receive
+import io.ktor.server.response.respond
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
 
@@ -10,7 +15,14 @@ fun Route.authRoutes() {
 	authenticate("auth-jwt"){
 		route("/login") {
 			post {
-
+				val loginUserDto = call.receive<LoginUserDto>()
+				val token = authService.login(loginUserDto)
+				if(token != null){
+					call.respond(token)
+				}
+				else{
+					call.respond("Invalid credentials")
+				}
 			}
 		}
 		route("/login-google") {
@@ -27,6 +39,20 @@ fun Route.authRoutes() {
 			post {
 
 			}
+		}
+	}
+	route("/register-student") {
+		post {
+			val registerStudentDto = call.receive<RegisterStudentDto>()
+			authService.registerStudent(registerStudentDto)
+			call.respond("Registration successful")
+		}
+	}
+	route("/register-company") {
+		post {
+			val registerCompanyDto = call.receive<RegisterCompanyDto>()
+			authService.registerCompany(registerCompanyDto)
+			call.respond("Registration successful")
 		}
 	}
 
