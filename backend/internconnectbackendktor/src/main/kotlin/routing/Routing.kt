2f8d17d3d4d11c8b557049194c5c8d974d5264df
LazarBojanic <bojanic.lazar50@gmail.com>
2025-10-27 03:00:11 +0100

    misc

--- backend/internconnectbackendktor/src/main/kotlin/routing/Routing.kt
+++ backend/internconnectbackendktor/src/main/kotlin/routing/Routing.kt
@@ -2,11 +2,17 @@ package com.internconnect.routing
 
 import io.ktor.server.application.*
 import io.ktor.server.resources.*
+import io.ktor.server.response.respondText
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
 	install(Resources)
 	routing {
+		route("/"){
+			get {
+				call.respondText("hello world")
+			}
+		}
 		auditLogRoutes()
 		userRoutes()
 		emailVerificationRoutes()
